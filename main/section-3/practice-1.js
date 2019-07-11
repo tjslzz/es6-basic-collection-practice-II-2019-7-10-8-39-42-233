db1f01f895d73145eb93d30595b3328ec094da15
JERRY LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIJE9@OOCL.COM>
2019-07-11 08:24:58 +0800

    finish section-3 practice-1

--- main/section-3/practice-1.js
+++ main/section-3/practice-1.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports = function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  let result = new Array();
+  collectionA.map((item)=>{
+    if(objectB.value.includes(item.key)){
+      item.count --;
+    }
+    result.push(item);
+  })
+  return result;
 }
